b1f4676755ced9c51beae96740bc924c5a165ea0
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-10-14 21:05:10 -0500

    get git vue data to return in chart

--- index.js
+++ index.js
@@ -1,9 +1,52 @@
 /* global Chart, axios */
 
-axios.get('https://api.github.com/')
+axios.get('https://api.github.com/repos/vuejs/vue')
   .then(function (response) {
     // handle success
-    console.log(response);
+    console.log(response.data);
+    let counts = {};
+    counts.stars = response.data.stargazers_count;
+    counts.watchers = response.data.watchers_count;
+    counts.forks = response.data.forks_count;
+    console.log("stars", counts.stars);
+    console.log("wathers", counts.watchers);
+    console.log("forks", counts.forks);
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Stars', 'Watchers', 'Forks', 'Popularity'],
+        datasets: [{
+          label: '# of Votes',
+          data: [counts.stars, counts.watchers, counts.forks],
+          backgroundColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 2)',
+            'rgba(54, 162, 235, 2)',
+            'rgba(255, 206, 86, 2)',
+            'rgba(75, 192, 192, 2)',
+            'rgba(153, 102, 255, 2)',
+            'rgba(255, 159, 64, 2)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
   })
   .catch(function (error) {
     // handle error
@@ -12,39 +55,3 @@ axios.get('https://api.github.com/')
   .then(function () {
     // always executed
   });
-
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Stars', 'Watchers', 'Forks', 'Popularity'],
-    datasets: [{
-      label: '# of Votes',
-      data: [5, 19, 3, 8, 22, 13],
-      backgroundColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 2)',
-        'rgba(54, 162, 235, 2)',
-        'rgba(255, 206, 86, 2)',
-        'rgba(75, 192, 192, 2)',
-        'rgba(153, 102, 255, 2)',
-        'rgba(255, 159, 64, 2)'
-      ],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
-});
